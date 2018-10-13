bc667eaf6b9c578593aaad67c873ea3df289991c
Clarisa Leu-Rodriguez <clarisarodriguezaz@gmail.com>
2018-10-13 10:50:51 -0700

    Updated logo page.

--- src/components/dashboard.js
+++ src/components/dashboard.js
@@ -1,17 +1,59 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { withRouter } from 'react-router';
+import { Input, Menu, Segment } from 'semantic-ui-react';
+import form from 'semantic-ui-react';
 
 // Profile Page + Settings
 export default class dashboard extends Component {
-  state = { activeItem: 'Profile' };
+  state = { activeItem: 'Dashboard' };
   handleItemClick = (e, { name }) => this.setState({ activeItem: name });
 
   render() {
     const { activeItem } = this.state;
     return (
       <div>
-        Helllllooooooooo.
+        {/* Menu */}
+        <Menu pointing>
+          <Menu.Item
+            name="Dashboard"
+            active={activeItem === 'Dashboard'}
+            onClick={() => this.props.history.push('/Dashboard')}
+          />
+          <Menu.Item
+            name="Messages"
+            active={activeItem === 'Messages'}
+            onClick={() => this.props.history.push('/Messages')}
+          />
+          <Menu.Item
+            name="Friends"
+            active={activeItem === 'Friends'}
+            onClick={() => this.props.history.push('/Friends')}
+          />
+          <Menu.Item
+            name="Profile"
+            active={activeItem === 'Profile'}
+            onClick={() => this.props.history.push('/Profile')}
+          />
+          <Menu.Item
+            name="Settings"
+            active={activeItem === 'Settings'}
+            onClick={() => this.props.history.push('/Settings')}
+          />
+          <Menu.Menu position="right">
+            <Menu.Item>
+              <Input icon="search" placeholder="Search..." />
+            </Menu.Item>
+          </Menu.Menu>
+        </Menu>
+
+        {/* Form to update statuses */}
+        <div class="ui form">
+          <div class="field">
+            <label>User Input</label>
+            <input type="text" />
+          </div>
+        </div>
       </div>
     );
   }
--- src/components/login.js
+++ src/components/login.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { withRouter } from 'react-router-dom';
 import 'semantic-ui-css/semantic.min.css';
-import { Header } from 'semantic-ui-react';
 import form from 'semantic-ui-react';
 import '../styles.css';
 
@@ -16,33 +15,38 @@ export default class login extends Component {
     return (
       <div>
         <div className="child">
-          <Header as="h1">TITLE</Header>
-          <form class="ui form">
-            <div class="field">
-              <label>First Name</label>
-              <input type="text" name="first-name" placeholder="First Name" />
-            </div>
-            <div class="field">
-              <label>Last Name</label>
-              <input type="text" name="last-name" placeholder="Last Name" />
-            </div>
-            <div class="ui checkbox">
-              <input type="checkbox" tabindex="0" class="hidden" />
-              <label className="agreement">
-                I Agree to the Terms and Conditions
-              </label>
-            </div>
-            <div className="button-style">
-              <button
-                class="ui basic button"
-                type="submit"
-                onClick={() => this.props.history.push('/Profile')}
-              >
-                <i class="icon user" />
-                Sign In
-              </button>
-            </div>
-          </form>
+          {/* Logo */}
+          <img className="logo " src={require('../images/logo2.png')} />
+
+          {/* Form to sign in */}
+          <div className="form-contain">
+            <form class="ui form">
+              <div class="field">
+                <label>First Name</label>
+                <input type="text" name="first-name" placeholder="First Name" />
+              </div>
+              <div class="field">
+                <label>Last Name</label>
+                <input type="text" name="last-name" placeholder="Last Name" />
+              </div>
+              <div class="ui checkbox">
+                <input type="checkbox" tabindex="0" class="hidden" />
+                <label className="agreement">
+                  I Agree to the Terms and Conditions
+                </label>
+              </div>
+              <div className="button-style">
+                <button
+                  class="ui basic button"
+                  type="submit"
+                  onClick={() => this.props.history.push('/Profile')}
+                >
+                  <i class="icon user" />
+                  Sign In
+                </button>
+              </div>
+            </form>
+          </div>
         </div>
       </div>
     );
